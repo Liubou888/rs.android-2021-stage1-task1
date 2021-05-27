b93ba82d94eaffe3f9b05d600cfa3ea952d23d2b
Liubou <82825264+Liubou888@users.noreply.github.com>
2021-05-28 01:23:23 +0300

    complete task 2

--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -5,6 +5,16 @@ class BillCounter {
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+        var bactual = 0
+        var bcharged = 0
+        for ((index, costOfItem) in bill.withIndex()) {
+            if (index != k) bactual += costOfItem
+            bcharged += costOfItem
+        }
+        if (b <= bactual / 2) return ("Bon Appetit")
+        return "${b - (bactual / 2)}"
     }
 }
+
+
+
