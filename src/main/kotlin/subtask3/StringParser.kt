ce0d157fe8c3993512e2db3c0054e367c92a270f
Liubou <82825264+Liubou888@users.noreply.github.com>
2021-05-28 01:24:02 +0300

    complete task 3

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -4,6 +4,26 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
-    }
+        val brackets = listOf('(','<','[',')','>',']')
+        val result = ArrayList<String>()
+        val s = StringBuilder(inputString)
+        for(i in s.length-1 downTo 0){
+            if(s[i]in brackets.subList(0,3)){
+                var last = 0
+                for(letter in i until s.length) {
+                    if (s[letter] == brackets[brackets.indexOf(inputString[i]) + 3]) {
+                        last = letter
+                        s[i] = ' '
+                        s[last] = ' '
+                        break
+                    }
+                }
+                result.add(0,inputString.substring(i+1,last))
+
+
+            }
+        }
+        return result.toTypedArray()
+
+}
 }
